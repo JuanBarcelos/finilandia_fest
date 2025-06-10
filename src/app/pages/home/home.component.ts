f2c4e9713cf9668ba1993bd79af0ce98baff9ae7
Juan Barcelos <juanbarcelos@outlook.com.br>
2025-06-10 11:02:14 -0300

    feat: create section event

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { PrimaryButtonComponent } from "../../components/primary-button/primary-button.component";
+import { EventCardComponent } from "../../components/event-card/event-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, PrimaryButtonComponent],
+  imports: [NavbarComponent, PrimaryButtonComponent, EventCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
